d4cff828683cbe64f1045ba70e51ee4816697db6
BeaAlumFlor <becero@alumnatflorida.es>
2024-12-16 21:41:19 +0100

    Primeras rutas creadas

--- src/index.js
+++ src/index.js
@@ -8,3 +8,35 @@ app.listen(app.get("port"), leerPuerto()); //Esta atento a lo que pasa en el pue
 function leerPuerto(){
     console.log(`Hola desde el puerto ${app.get("port")}`); //nos da el número de puerto
 }
+
+
+//Rutas
+app.get("/", function(req, res) { //primer parametro nombre de ruta, el segundo lo que quiero que haga (funcion anonima, solo se usa aqui, no necesita nombre)
+    console.log(`Aviso: alguien ha accedido al indice en el servidor`);
+    res.send(`Hola, ¡Bienvenido!`) 
+});
+
+app.get("/rutaget1", function(req, res) { 
+console.log(`Aviso: alguien ha accedido a la 1ª ruta get`);
+res.send(`Hola soy la ruta 1ª`)
+});
+
+app.get("/rutaget2", (req, res) => { //función flecha
+    console.log(`Aviso: alguien ha accedido a la 2ª ruta get`);
+    res.send(`Hola soy la ruta 2ª`)
+});
+
+app.get("/rutaget3", (req, res) => { //función flecha
+    console.log(`Aviso: alguien ha accedido a la 3ª ruta get`);
+    res.send(`Hola soy la ruta 3ª`)
+});
+
+app.get("/rutaget4", (req, res) => { //función flecha
+    console.log(`Aviso: alguien ha accedido a la 4ª ruta get`);
+    res.send(`Hola soy la ruta 4ª`)
+});
+
+app.get("/rutaget5", (req, res) => { //función flecha
+    console.log(`Aviso: alguien ha accedido a la 5ª ruta get`);
+    res.send(`Hola soy la ruta 5ª`)
+});
