6ddd8d24b9f44103816b468085af62d9c57955c3
BeaAlumFlor <becero@alumnatflorida.es>
2024-12-16 19:38:03 +0100

    Inicio del servidor

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const express = require("express"); //Variable que almacena a express ("como un objeto")
+const app= express(); //Se inicializa el "objeto" express
+
+app.set("port", process.env.PORT || 8080); //puerto de entrada, permitirá ver en internet - 8080 puerto de testeo
+
+app.listen(app.get("port"), leerPuerto()); //Esta atento a lo que pasa en el puerto
+
+function leerPuerto(){
+    console.log(`Hola desde el puerto ${app.get("port")}`); //nos da el número de puerto
+}
